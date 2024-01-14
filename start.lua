31c56cd092946a2e01c140c7ff7cc404c38460ad
JuLY LION <106941080+JuLY-LION@users.noreply.github.com>
2024-01-13 23:43:09 -0500

    Update start.lua

--- start.lua
+++ start.lua
@@ -6,9 +6,15 @@ end
 
 userinput("Buckshot Lualette (Alpha)")
 
-username = userinput("Got a name?")
+username = userinput("> Got a name?")
 username = username == "" and "Username"
 userinput("Welcome aboard, "..username..".")
 userinput("I hope you weren't expecting more formalities. It's too soon for that.")
+shotgun = {}
+shells = {}
 
-userinput("-------\nROUND 1\n2 lives, no items.\n-------")
+--round 1
+print("---------\nROUND 1/4\n---------")
+userinput("2 lives, no items.")
+
+
